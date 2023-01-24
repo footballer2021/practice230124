6b9ac4697bd49d618ab10f3b91d64cf381719dc5
footballer2021 <soccerzuki2013@gmail.com>
2023-01-24 14:43:21 +0900

    first message

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './routes/Router';
 
-function App() {
+const ROUTER_BASENAME = process.env.NODE_ENV === 'development' ? '/' : '/practice230124';
+
+const App:React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={ROUTER_BASENAME}>
+      <Router />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/counter1/eachPage/Counter1.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../../components/Header'
+
+const Counter1 = () => {
+  return (
+    <>
+        <Header/>
+        <h1>Counter1</h1>
+    </>
+  )
+}
+
+export default Counter1
